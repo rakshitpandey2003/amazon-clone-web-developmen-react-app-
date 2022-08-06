8dd2fb17926d9c0cb649d9f7c807deb416337d00
rakshitpandey2003 <110710174+rakshitpandey2003@users.noreply.github.com>
2022-08-06 10:53:59 +0530

    Add files via upload

---
+++ untitled folder 3/react-challenge-amazon-clone-master/build/precache-manifest.e719586f9b974ca36e7ae7c02349f997.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "381489f5d5cb53c4311dbaaceb52cafa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ec4d7bf048b4e5d2640",
+    "url": "/static/css/main.78c7c615.chunk.css"
+  },
+  {
+    "revision": "66057293e9aff8f36eaf",
+    "url": "/static/js/2.1152b9cd.chunk.js"
+  },
+  {
+    "revision": "2b587c35db5df3608932eb682dfa83dc",
+    "url": "/static/js/2.1152b9cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ec4d7bf048b4e5d2640",
+    "url": "/static/js/main.92458a2d.chunk.js"
+  },
+  {
+    "revision": "92a146e5ab9e0f9e785b",
+    "url": "/static/js/runtime-main.90140090.js"
+  }
+]);
